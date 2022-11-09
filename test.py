37097eae203d2032ad516205f3e8845d87a08b66
hectpumhui <forsoctask@gmail.com>
2022-11-09 21:19:58 +0200

    begin simple prog

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
 print('Hello there!!@$@$@$@%@%!')
+# simple program
+num = int(input(f'Enter first number: '))
+num_2 = int(input(f'Enter second number : '))
+do = input(f'Enter command - "+""-""/""*""**": ')
